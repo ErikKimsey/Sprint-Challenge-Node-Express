bf3a181be477002d3131ac73ae39c0d584078cab
Erik Kimsey <erik@erikkimsey.com>
2018-08-31 11:24:07 -0400

    yarn init. Created server file, package.json, installed 3rd party middleware and fired up server on 9000

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan());
+app.use(helmet());
+
+
+
+app.listen(9000, () => console.log("Listening on 9000"));
