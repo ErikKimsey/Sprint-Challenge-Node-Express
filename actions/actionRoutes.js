4b75cf32b70236289b3b2a67b8a6aa064624e29f
Erik Kimsey <erik@erikkimsey.com>
2018-08-31 12:21:26 -0400

    Action routes... get, get with id, post, and put are functioning as expected

---
+++ actions/actionRoutes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const actionModel = require('../data/helpers/actionModel.js');
+
+router.get('/', (req, res)=>{
+    actionModel.get()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(500).json({message: `There was a problem fetching actions.`})
+        });
+});
+
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    actionModel.get(id)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(500).json({ message: `There was a problem fetching actions.` })
+        });
+});
+
+router.post('/', (req, res) => {
+    let action = req.body;
+    actionModel.insert(action)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({ message: `There was a problem creating an action.` })
+        });
+});
+
+router.put('/:id', (req, res) => {
+    console.log(req.body);
+    
+    actionModel.update(req.params.id, req.body)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({ message: `There was a problem updating this action.` })
+        });
+});
+
+
+
+module.exports = router;
+
+
+/**
+ * 
+id: number, no need to provide it when creating posts, the database will automatically generate it.
+project_id: number, required, must be the id of an existing project.
+description: string, up to 128 characters long, required.
+notes: string, no size limit, required. Used to record additional notes or requirements to complete the action.
+completed: boolean to indicate if the action has been completed, not required
+ */
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const actionRoutes = require('./actions/actionRoutes.js');
+const projectRoutes = require('./projects/projectRoutes.js');
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan());
 app.use(helmet());
+
+app.use('/actions', actionRoutes);
 
 
 
